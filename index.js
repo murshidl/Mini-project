ba9f3e1c8ddc9f50f25b06aed8d4369fff4366e7
murshidl <murshidali501@gmail.com>
2025-06-10 13:18:00 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+inquirer
+    .prompt([
+        {
+            message: 'Please enter a URL to generate a QR code:',
+            name: 'url',
+        }
+    ])
+    .then((answers) => {
+        const url = answers.url;
+        var qr_svg = qr.image(url, );
+        qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
